b95a46be0e156d7425f9b8ec759fcbb30ddfa2f3
Trung047 <trunggn123@gmail.com>
2026-06-08 15:57:40 +0700

    UserRepository.java

---
+++ Database/UserRepository.java
@@ -0,0 +1,8 @@
+package database;
+
+public class UserRepository {
+    public void saveUser(String username, String password) {
+        // Code thêm người dùng mới vào database
+        System.out.println("Đã lưu thông tin user: " + username);
+    }
+}
