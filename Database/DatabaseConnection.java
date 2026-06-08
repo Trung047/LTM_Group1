a069ce48b885ab33f1a39bc395c4284414bd6285
Trung047 <trunggn123@gmail.com>
2026-06-08 16:12:04 +0700

    Update DatabaseConnection.java

--- Database/DatabaseConnection.java
+++ Database/DatabaseConnection.java
@@ -1,8 +1,24 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
-    // Viết code kết nối CSDL ở đây
-    public void connect() {
-        System.out.println("Đang kết nối tới Database...");
+    private static final String URL = "jdbc:sqlserver://localhost:1433;"
+            + "databaseName=ChatAppDB;"
+            + "encrypt=true;trustServerCertificate=true;";
+    
+    private static final String USER = "sa";       
+    private static final String PASSWORD = "123456"; 
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Không tìm thấy Microsoft JDBC Driver! Hãy add file .jar vào thư viện.");
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
