f9556e81ddde4cbcc1abb38d88f65592de62d65f
Trung047 <trunggn123@gmail.com>
2026-06-08 16:03:00 +0700

    Update UserRepository.java

--- Database/UserRepository.java
+++ Database/UserRepository.java
@@ -2,7 +2,6 @@ package database;
 
 public class UserRepository {
     public void saveUser(String username, String password) {
-        // Code thêm người dùng mới vào database
         System.out.println("Đã lưu thông tin user: " + username);
     }
 }
