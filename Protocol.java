c054180266b2b39543e675c9cf66bf6e38edfe46
kietqp <kietpq3616@ut.edu.vn>
2026-06-19 12:52:55 +0700

    Delete Protocol.java

--- Protocol.java
+++
@@ -1,8 +0,0 @@
-// Định nghĩa các giao thức sử dụng trong chương trình
-public class Protocol{
-
-    public static final String GROUP="GROUP"; // chat nhóm
-    public static final String PRIVATE="PRIVATE"; // chat riêng
-    public static final String JOIN="JOIN"; // tham gia phòng
-    public static final String LEAVE="LEAVE"; // rời phòng
-}
